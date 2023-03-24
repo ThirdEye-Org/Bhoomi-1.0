92805fde7b42c7137709bc3156ee638d6a4bf67e
Ayush Baliyan <ayush.3316403221@ipu.ac.in>
2023-03-25 03:20:07 +0530

    added gltf

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-// import { Model } from './Scene';
 import ThreeScene from './ThreeScene';
 
 function App() {
   return (
     <div className="App">
       <ThreeScene/>
-      {/* <Model/> */}
     </div>
   );
 }
