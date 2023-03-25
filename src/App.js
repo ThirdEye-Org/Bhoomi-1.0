16cdbe407868cbc6bee2004419a3da61969b10ad
Ayush Baliyan <ayush.3316403221@ipu.ac.in>
2023-03-25 13:56:22 +0530

    Home page done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import ThreeScene from "./ThreeScene";
 import React from "react";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/home";
 
 export const userContext = React.createContext();
 
@@ -13,26 +15,32 @@ function App() {
   const [email, setEmail] = React.useState(null);
 
   return (
-    <userContext.Provider
-      value={{
-        loggedIn,
-        setLoggedIn,
-        initialized,
-        setInitialized,
-        account,
-        setAccount,
-        email,
-        setEmail,
-      }}
-    >
-      <div>
-        {/* <ThreeScene/> */}
-        <Navbar />
-        <Profile />
-        {/* <Model/> */}
-        {/* <Home /> */}
-      </div>
-    </userContext.Provider>
+    <Router>
+      <userContext.Provider
+        value={{
+          loggedIn,
+          setLoggedIn,
+          initialized,
+          setInitialized,
+          account,
+          setAccount,
+          email,
+          setEmail,
+        }}
+      >
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            {/* <Route path="/" element={<Home/>}/> */}
+            {/* <Profile /> */}
+            {/* <Model/> */}
+            {/* <Home /> */}
+          </Routes>
+        </div>
+      </userContext.Provider>
+    </Router>
   );
 }
 
