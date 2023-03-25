296cb1f652f3537125fbf9d78a02213966e62a1a
Ayush Baliyan <ayush.3316403221@ipu.ac.in>
2023-03-26 04:04:09 +0530

    profile card

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,7 +4,7 @@ import ProfileHero from './profileHero'
 
 function Profile() {
   return (
-      <div className=' container mx-auto flex flex-row space-x-12 pt-4'>
+      <div className=' container mx-auto flex flex-row  pt-4'>
       <ProfileCard />
       <ProfileHero/>
     </div>
